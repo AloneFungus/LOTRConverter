4d95526e57a06e9622493d678ad1e1ca7eeaf6cf
R ARUNACHALAM <chandrikaarun1958@gmail.com>
2024-08-10 20:08:00 +0530

    Initial Code Setup

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Arunachalam on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
